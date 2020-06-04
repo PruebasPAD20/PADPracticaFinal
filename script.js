4a280b88b00663bccec6c25942ab514c0ae8b4cd
RubenSC-UCM <44059432+RubenSC-UCM@users.noreply.github.com>
2020-06-04 23:26:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ var creandoTarea = false;
 var editantoTarea = false;
 var tarea = false;
 
-
-
-	// si nuestro navegador ejecuta service workers
+// si nuestro navegador ejecuta service workers
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('script.js').then(reg => {
@@ -16,70 +14,7 @@ var tarea = false;
             })
         })
     }
-
-
-
-	var CACHE_NAME = 'pad-cache-v1';
-	var urlsToCache = [
-		'/',
-		'/index.html',
-		'/estilo.css',
-		'/script.js',
-		'/manifest.json'
-	];
-
-	self.addEventListener('install', function(event) {
-	  // Perform install steps
-	  event.waitUntil(
-		caches.open(CACHE_NAME)
-		  .then(function(cache) {
-			console.log('Opened cache');
-			return cache.addAll(urlsToCache);
-		  })
-	  );
-	});
-
-
-
-	self.addEventListener('fetch', function(event) {
-		event.respondWith(
-			caches.match(event.request)
-			.then(function(response) {
-				// Cache hit - return response
-				if (response) {
-				  return response;
-				}
-
-				// IMPORTANT: Clone the request. A request is a stream and
-				// can only be consumed once. Since we are consuming this
-				// once by cache and once by the browser for fetch, we need
-				// to clone the response.
-				var fetchRequest = event.request.clone();
-
-				return fetch(fetchRequest).then(
-				  function(response) {
-					// Check if we received a valid response
-					if(!response || response.status !== 200 || response.type !== 'basic') {
-					  return response;
-					}
-
-					// IMPORTANT: Clone the response. A response is a stream
-					// and because we want the browser to consume the response
-					// as well as the cache consuming the response, we need
-					// to clone it so we have two streams.
-					var responseToCache = response.clone();
-
-					caches.open(CACHE_NAME)
-					  .then(function(cache) {
-						cache.put(event.request, responseToCache);
-					  });
-
-					return response;
-				  }
-				);
-			})
-		);
-	});
+	
 
 	
 function cargaTareas(){
@@ -232,7 +167,6 @@ function guardarEdicion(id, texto, boton){
 function pruebas(text){
 	alert(text.id);
 }
-
 
 btnTarea.onclick = function(){if(creandoTarea==false){nuevaTarea()}};
 cargaTareas();
